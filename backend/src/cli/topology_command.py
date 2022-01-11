ff24756fad986f51f27f5a1344d38dacce06d8da
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-01-11 14:21:33 +0700

    delete

--- backend/src/cli/topology_command.py
+++
@@ -1,7 +0,0 @@
-class TopologyCommand:
-    def __init__(self, topology):
-        self.topology = topology
-
-    def handle_command(self, command):
-        """ Handle topology command"""
-        pass
