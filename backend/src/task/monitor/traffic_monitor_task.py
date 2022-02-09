d9b2b37d688ff18141715693c299b9089e7ebae1
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-02-09 22:17:36 +0700

    Update traffic_monitor_task.py

--- backend/src/task/monitor/traffic_monitor_task.py
+++ backend/src/task/monitor/traffic_monitor_task.py
@@ -47,7 +47,7 @@ class TrafficMonitorTask:
         self.path_finder.update_graph()
         link_info = self.path_finder.get_links()
         print('--------------------')
-        print(self.path_finder.graph)
+        print(self.path_finder.shortest_by_manage_ip('192.168.1.1', '192.168.4.2'))
         print('--------------------')
         # self.path_finder.save_graph_img()
 
