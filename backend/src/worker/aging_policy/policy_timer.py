940120488e1bb875daf85ebff4164b773b57c8b7
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-02-09 14:45:57 +0700

    test graph

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -54,25 +54,21 @@ class TimerPolicyWorker:
 
     def run(self):
         while True:
-            print('fdfsdfsdfsdfsdfsfs')
-            print('fdfsdfsdfsdfsdfsfs')
-            print('fdfsdfsdfsdfsdfsfs')
-            print('fdfsdfsdfsdfsdfsfs')
-            # self.flow = self.client.sdn01.flow_routing.find()
-            # for obj in self.flow:
-            #     if len(obj) == 15:
-            #         key = {
-            #             'ipv4_src_addr' : obj['src_ip'],
-            #             'l4_src_port' : obj['src_port'],
-            #             'ipv4_dst_addr' : obj['dst_ip'],
-            #             'l4_dst_port' : obj['dst_port'],
-            #             }
-            #         info = {
-            #             'ipv4_src_addr_wildcard' : obj['src_wildcard'],
-            #             'ipv4_dst_addr_wildcard' : obj['dst_wildcard'],
-            #             'flow_id' : obj['flow_id']
-            #         }
-            #         # key = {i:obj[i] for i in ['src_ip', 'src_port', 'dst_ip', 'dst_port', 'src_wildcard', 'dst_wildcard', 'flow_id']}
-            #         if obj['aging_time']:
-            #             Counter(key, info, self.client, obj['aging_time']).start()
+            self.flow = self.client.sdn01.flow_routing.find()
+            for obj in self.flow:
+                if len(obj) == 15:
+                    key = {
+                        'ipv4_src_addr' : obj['src_ip'],
+                        'l4_src_port' : obj['src_port'],
+                        'ipv4_dst_addr' : obj['dst_ip'],
+                        'l4_dst_port' : obj['dst_port'],
+                        }
+                    info = {
+                        'ipv4_src_addr_wildcard' : obj['src_wildcard'],
+                        'ipv4_dst_addr_wildcard' : obj['dst_wildcard'],
+                        'flow_id' : obj['flow_id']
+                    }
+                    # key = {i:obj[i] for i in ['src_ip', 'src_port', 'dst_ip', 'dst_port', 'src_wildcard', 'dst_wildcard', 'flow_id']}
+                    if obj['aging_time']:
+                        Counter(key, info, self.client, obj['aging_time']).start()
             time.sleep(2)
