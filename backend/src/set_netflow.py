15227547832409cf8356b7be78c48faca973227b
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-03-18 19:47:42 +0700

    Update set_netflow.py

--- backend/src/set_netflow.py
+++ backend/src/set_netflow.py
@@ -61,9 +61,8 @@ def init_netflow_setting(devices, management_ip):
     problem_devices = []
     print('Hi I AM here')
     print('Hi I AM here')
-    print('Hi I AM here')
-    print('Hi I AM here')
     print(devices)
     for device in devices:
         problem_devices += set_netflow_worker().run(device, management_ip)
+        print('#$######$$$$$$$$$$$$$$$$$$$$$$$$')
     return problem_devices
