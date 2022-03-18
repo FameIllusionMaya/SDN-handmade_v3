e0e591dba262bc91eb392db2712e5479707a8498
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-03-18 19:52:48 +0700

    Update set_netflow.py

--- backend/src/set_netflow.py
+++ backend/src/set_netflow.py
@@ -50,7 +50,7 @@ class set_netflow_worker(Thread):
             print('Hi I AM here')
             print('Hi I AM here')
             device_repository = repository.get("device")
-            device_repository.set_snmp_is_connect_by_mgmt_ip(management_ip, True)
+            device_repository.set_netflow_is_connect_by_mgmt_ip(management_ip, True)
             print('heyqqqqqqqqqqqqqqqqqqqqqqq')
             ssh.close()
             return []
