bda3f18868199b4ac62f46d18960207319c7a39e
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-04-06 22:53:23 +0700

    test

--- backend/src/task/monitor/traffic_monitor_task.py
+++ backend/src/task/monitor/traffic_monitor_task.py
@@ -53,12 +53,12 @@ class TrafficMonitorTask:
                 if str(flow['_id']) in running_flow_id:
                     flow_data = {
                         'flow_id':str(flow['_id']),
-                        'in_bytes':flow['in_bytes'],
+                        'in_pkts':flow['in_pkts'],
                         'src_ip': flow['ipv4_src_addr'] + '/' + str(flow['src_mask']),
                         'dst_ip': flow['ipv4_dst_addr'] + '/' + str(flow['dst_mask'])
                     }
                     problem_flow.append(flow_data)
-            problem_flow_sorted = sorted(problem_flow, key=lambda d: d['in_bytes'], reverse=True)
+            problem_flow_sorted = sorted(problem_flow, key=lambda d: d['in_pkts'], reverse=True)
             return problem_flow_sorted
 
         def do_loadbalance(problem_flow_sorted, link):
@@ -77,8 +77,8 @@ class TrafficMonitorTask:
                             # print('Interface not setting IP ignore it')
                             pass
 
-            def check_dup_link(path, link_src_dst, all_link):
-                link_list = []
+            def check_dup_link(path, link_src_dst, all_link, flow):
+                link_path_list = []
                 for node_index in range(len(path)):
                     if node_index + 1 != len(path):
                         src = path[node_index]
@@ -86,20 +86,21 @@ class TrafficMonitorTask:
                         for link in all_link:
                             print(src, dst, link['dst_node_ip'], link['src_node_ip'])
                             if (src == link['src_node_ip'] or src == link['dst_node_ip']) and (dst == link['src_node_ip'] or dst == link['dst_node_ip']):
-                                print('@@@@@@@@@@@@@@@@@@@@')
-                                print('YES')
-                                print(link['_id'])
-                                print('@@@@@@@@@@@@@@@@@@@@')
+                                in_flow = int(max(link['src_in_use'], link['dst_out_use'])) + flow['in_pkts']
+                                out_flow = int(max(link['src_out_use'], link['dst_in_use'])) + flow['in_pkts']
+                                utilization_percent = round(decimal.Decimal((in_flow + out_flow)/(link['link_min_speed'])), 5)
+                                link_path_list.append(link['_id'])
                     if (src == link_src_dst[0] and dst == link_src_dst[1]) or (src == link_src_dst[1] and dst == link_src_dst[0]):
-                        return [True, link_list]
-                return [False, link_list]
+                        return [True, link_path_list]
+                print(link_path_list)
+                return [False, link_path_list]
 
 
             for flow in problem_flow_sorted:
                 """
-                problem_flow = {
+                flow = {
                 'flow_id':str(flow['_id']),
-                'in_bytes': '12345',
+                'in_pkts': '12345',
                 'src_ip': '192.168.2.1/24',
                 'dst_ip': '192.168.1.1/24'
                 }
@@ -110,7 +111,7 @@ class TrafficMonitorTask:
                 all_path = requests.get("http://localhost:5001/api/v1/path/" + src_mmip + "," + dst_mmip).json()['paths']
                 print('====================')
                 for path in all_path:
-                    if not check_dup_link(path['path'], link['link_mmip'], all_link)[0]:
+                    if not check_dup_link(path['path'], link['link_mmip'], all_link, flow)[0]:
                         path_choice.append(path['path'])
                 print('====================')
                 print(path_choice)
