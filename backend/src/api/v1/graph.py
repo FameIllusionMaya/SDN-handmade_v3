914ab5bfb4fd99b7333d3e7b26f8af1efb4b1423
MeenPongpanit <61070124@kmitl.ac.th>
2022-04-16 12:47:12 +0700

    graph api data update

--- backend/src/api/v1/graph.py
+++ backend/src/api/v1/graph.py
@@ -6,18 +6,42 @@ from sanic.views import HTTPMethodView
 class GraphView(HTTPMethodView):
 
     def get(self, request):
-        data = loads(dumps(request.app.db['link_utilization'].get_all()))
+        links_data = loads(dumps(request.app.db['link_utilization'].get_all()))
+        # devices_data = loads(dumps(request.app.db['link_utilization'].get_all()))
+
         nodes = {}
         edges = {}
-        for link in data:
+        for link in links_data:
             src_node = link['src_node_hostname']
             dst_node = link['dst_node_hostname']
-            if src_node not in nodes:
-                nodes[src_node] = f'node{len(nodes)}'
-            if dst_node not in nodes:
-                nodes[dst_node] = f'node{len(nodes)}'
-            edges[f'edge{len(edges)}'] = {'source':nodes[src_node], 'target':nodes[dst_node], 'src_port':link['src_port'],  'dst_port':link['dst_port']}
-        nodes = {nodes[i]:{'name':i} for i in nodes}
+            nodes.update({
+                src_node:{
+                    'name': link['src_node_hostname'], 
+                    'management_ip': link['src_node_ip']
+                    }
+                })
+            nodes.update({
+                dst_node:{
+                    'name': link['dst_node_hostname'], 
+                    'management_ip': link['dst_node_ip']
+                    }
+                })
+            # if src_node not in nodes:
+            #     nodes[src_node] = f'node{len(nodes)}'
+            # if dst_node not in nodes:
+            #     nodes[dst_node] = f'node{len(nodes)}'
+            edges[f'edge{len(edges)}'] = {
+                'dst_if_ip':link['dst_if_ip'], 
+                'src_if_ip':link['src_if_ip'], 
+                'source':nodes[src_node]['name'], 
+                'target':nodes[dst_node]['name'], 
+                'src_port':link['src_port'],  
+                'dst_port':link['dst_port']
+            }
+        # nodes = {
+        #         nodes[i]:{
+        #             'name':i
+        #         } for i in nodes}
         graph = {"nodes":nodes, "edges":edges}
         flows = request.app.db['flow_stat'].get_all().sort("in_bytes", -1)
         return json({"graph": graph, "status": "ok"})
