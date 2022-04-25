bde1831be9e720adbe99cddf290d932a9018da73
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-04-25 17:24:02 +0700

    test

--- Fame_test/tets.py
+++ Fame_test/tets.py
@@ -4,30 +4,32 @@ from ipaddress import *
 import random
 
 # controller_ip = '10.50.34.15'
-path = ['192.168.13.2', '192.168.12.1', '192.168.16.2', '192.168.67.2']
-# src_net = '192.168.100.0'
-# src_port = 'any'
-# src_wildcard = '0.0.0.255'
-# dst_net = '192.168.110.0'
-# dst_port = 'any'
-# dst_wildcard = '0.0.0.255'
-# action = [
-#     {'device_id':'62418a3c6b3e4f9c9c55a13c', 'action':2, 'data':'192.168.12.1'},
-#     {'device_id':'62418a2d6b3e4f9c9c55a109', 'action':2, 'data':'192.168.16.2'},
-#     {'device_id':'62418a526b3e4f9c9c55a1a4', 'action':2, 'data':'192.168.67.2'}
-#     ]
-# new_flow = {
-#     'name':'Yukari',
-#     'src_ip':src_net,
-#     'src_port':src_port,
-#     'src_subnet':src_wildcard, 
-#     'dst_ip':dst_net, 
-#     'dst_port':dst_port,
-#     'dst_subnet':dst_wildcard,
-#     'actions':action,
-#     'aging_time': 0
-#     }
-# requests.post("http://10.50.34.15:5001/api/v1/flow/routing", json=new_flow)
+# path = ['192.168.13.2', '192.168.12.1', '192.168.16.2', '192.168.67.2']
+
+
+
+src_net = '192.168.100.2'
+src_port = 'any'
+src_wildcard = '0.0.0.255'
+dst_net = '192.168.200.10'
+dst_port = 'any'
+dst_wildcard = '0.0.0.255'
+action = [
+    {'device_id':'62654a8955c5ba593f059d16', 'action':2, 'data':'192.168.23.2'},
+    {'device_id':'62654aab55c5ba593f059e00', 'action':2, 'data':'192.168.34.2'}
+    ]
+new_flow = {
+    'name':'Yukari',
+    'src_ip':src_net,
+    'src_port':src_port,
+    'src_subnet':src_wildcard, 
+    'dst_ip':dst_net, 
+    'dst_port':dst_port,
+    'dst_subnet':dst_wildcard,
+    'actions':action,
+    'aging_time': 0
+    }
+requests.post("http://10.50.34.15:5001/api/v1/flow/routing", json=new_flow)
 
 
 
@@ -92,30 +94,30 @@ path = ['192.168.13.2', '192.168.12.1', '192.168.16.2', '192.168.67.2']
 
 
 
-cash_tour = 0
-cash_single = 0
-for i in range(1000):
-    cash_tour -= 10
-    cash_single -= 10
+# cash_tour = 0
+# cash_single = 0
+# for i in range(1000):
+#     cash_tour -= 10
+#     cash_single -= 10
 
 
-    num_single = random.randint(1, 2)
-    num_tour = random.randint(1, 2)
+#     num_single = random.randint(1, 2)
+#     num_tour = random.randint(1, 2)
 
-    if num_single == 1:
-        cash_single += 18
-    else:
-        cash_single += 0
+#     if num_single == 1:
+#         cash_single += 18
+#     else:
+#         cash_single += 0
     
-    if num_tour == 1:
-        num_tour2 = random.randint(1, 2)
-        if num_tour2 == 1:
-            cash_tour += 27
-        else:
-            cash_tour += 9
-    else:
-        cash_tour -= 10
-    print(num_single, cash_single)
+#     if num_tour == 1:
+#         num_tour2 = random.randint(1, 2)
+#         if num_tour2 == 1:
+#             cash_tour += 27
+#         else:
+#             cash_tour += 9
+#     else:
+#         cash_tour -= 10
+#     print(num_single, cash_single)
 
-print(cash_tour, cash_single)
+# print(cash_tour, cash_single)
     
