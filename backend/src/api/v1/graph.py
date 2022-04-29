a491a5befcdc7d740ac34e6ca298ef9ca410f957
MeenPongpanit <61070124@kmitl.ac.th>
2022-04-29 21:06:40 +0700

    Update graph.py

--- backend/src/api/v1/graph.py
+++ backend/src/api/v1/graph.py
@@ -96,6 +96,7 @@ class GraphView(HTTPMethodView):
         edges = {}
 
         for device in devices_data:
+            print(device)
             device_id = str(device['_id'])
             management_ip = device['management_ip']
             device_type = device['type']
