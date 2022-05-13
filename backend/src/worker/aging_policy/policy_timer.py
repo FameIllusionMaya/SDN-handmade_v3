9ac60548b3e0cef0cb91aea1d91ef749d6317467
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-05-13 21:32:18 +0700

    Update policy_timer.py

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -72,6 +72,7 @@ class Counter(Thread):
 class TimerPolicyWorker(threading.Thread):
 
     def __init__(self):
+        print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
         threading.Thread.__init__(self)
         self.client = MongoClient('localhost', 27017)
         self.name = 'agingtime-sv'
