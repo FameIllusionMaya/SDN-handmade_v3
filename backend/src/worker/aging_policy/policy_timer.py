c0e4bbeba22bcb6eb17126b9d5afd2afcc379f6a
Purinut Jitmanas <38550312+FameIllusionMaya@users.noreply.github.com>
2022-05-14 09:41:42 +0700

    a

--- backend/src/worker/aging_policy/policy_timer.py
+++ backend/src/worker/aging_policy/policy_timer.py
@@ -112,16 +112,13 @@ class TimerPolicyWorker(threading.Thread):
                         'l4_src_port' : obj['src_port'],
                         'ipv4_dst_addr' : obj['dst_ip'],
                         'l4_dst_port' : obj['dst_port'],
-                        }
-                    info = {
                         'ipv4_src_addr_wildcard' : obj['src_wildcard'],
                         'ipv4_dst_addr_wildcard' : obj['dst_wildcard'],
                         'flow_id' : obj['flow_id']
                     }
                     print('HUEHUEHUE')
-                    key = {i:obj[i] for i in ['src_ip', 'src_port', 'dst_ip', 'dst_port', 'src_wildcard', 'dst_wildcard', 'flow_id']}
                     if obj['aging_time']:
-                        Counter(key, info, self.client, obj['aging_time']).start()
+                        Counter(key, self.client, obj['aging_time']).start()
             time.sleep(10)
 
 
