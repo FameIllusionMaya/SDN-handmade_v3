2f981f99d009e5c2e1ca7fe8957e0e9ec6fd3866
MeenPongpanit <61070124@kmitl.ac.th>
2022-06-01 20:32:58 +0700

    Update set_netflow.py

--- backend/src/set_netflow.py
+++ backend/src/set_netflow.py
@@ -10,34 +10,34 @@ client = MongoClient('localhost', 27017)
 
 class set_netflow_worker(Thread):
     def run(seld, device, controller_ip):
-        try:
-            if device['is_netflow']:
-                return []
-            
-            device_shell_info = {
-                'device_type': device['type'],
-                'host': device['management_ip'],
-                'username': device['ssh_info']['username'],
-                'password': device['ssh_info']['password'],
-                'secret':  device['ssh_info']['secret'],
-                'port': device['ssh_info']['port']
-                }
-            
-            ssh = ConnectHandler(**device_shell_info)
-
-            # set netflow
-            interfaces = client.sdn01.device.find({'management_ip': device['management_ip']}, {'_id':0, 'interfaces': 1})
-            cmds = generate_netflow_init_command(device['type'], controller_ip, interfaces)
-            ssh.enable()
-            ssh.send_config_set(cmds)
-
-            device_repository = repository.get("device")
-            device_repository.set_netflow_is_connect_by_mgmt_ip(device['management_ip'], True)
-            ssh.close()
+        # try:
+        if device['is_netflow']:
             return []
+        
+        device_shell_info = {
+            'device_type': device['type'],
+            'host': device['management_ip'],
+            'username': device['ssh_info']['username'],
+            'password': device['ssh_info']['password'],
+            'secret':  device['ssh_info']['secret'],
+            'port': device['ssh_info']['port']
+            }
+        
+        ssh = ConnectHandler(**device_shell_info)
 
-        except:
-            return [device['management_ip']]
+        # set netflow
+        interfaces = client.sdn01.device.find({'management_ip': device['management_ip']}, {'_id':0, 'interfaces': 1})
+        cmds = generate_netflow_init_command(device['type'], controller_ip, interfaces)
+        ssh.enable()
+        ssh.send_config_set(cmds)
+
+        device_repository = repository.get("device")
+        device_repository.set_netflow_is_connect_by_mgmt_ip(device['management_ip'], True)
+        ssh.close()
+        return []
+
+        # except:
+            # return [device['management_ip']]
 
 def init_netflow_setting(devices, management_ip):
     problem_devices = []
