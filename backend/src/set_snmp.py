01be6a1cd2ab07337573f46ddf1483207a95eb9b
MeenPongpanit <61070124@kmitl.ac.th>
2022-06-01 20:07:17 +0700

    Update set_snmp.py

--- backend/src/set_snmp.py
+++ backend/src/set_snmp.py
@@ -11,29 +11,29 @@ client = MongoClient('localhost', 27017)
 
 class set_snmp_worker(Thread):
     def run(self, device):
-        try:
-            if device['is_snmp_connect'] and device['cdp_enable']:
-                return []
-
-            device_shell_info = {
-                    'device_type': device['type'],
-                    'host': device['management_ip'],
-                    'username': device['ssh_info']['username'],
-                    'password': device['ssh_info']['password'],
-                    'secret':  device['ssh_info']['secret'],
-                    'port': device['ssh_info']['port']
-                    }
-                
-            ssh = ConnectHandler(**device_shell_info)
-
-            # set netflow
-            cmds = generate_snmp_init_command(device['type'])
-
-            ssh.send_config_set(cmds)
-            ssh.close()
+        # try:
+        if device['is_snmp_connect'] and device['cdp_enable']:
             return []
-        except:
-            return [device['management_ip']]
+
+        device_shell_info = {
+                'device_type': device['type'],
+                'host': device['management_ip'],
+                'username': device['ssh_info']['username'],
+                'password': device['ssh_info']['password'],
+                'secret':  device['ssh_info']['secret'],
+                'port': device['ssh_info']['port']
+                }
+            
+        ssh = ConnectHandler(**device_shell_info)
+
+        # set netflow
+        cmds = generate_snmp_init_command(device['type'])
+
+        ssh.send_config_set(cmds)
+        ssh.close()
+        return []
+        # except :
+        #     return [device['management_ip']]
 
 
 def sleep(device_connection):
